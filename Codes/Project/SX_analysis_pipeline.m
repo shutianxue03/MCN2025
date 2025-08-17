f94feace0b4d2e7bcd4ab9bdbd3036a5841d3194
Shutian Xue <sx712@nyu.edu>
2025-08-16 20:10:39 -0400

    successfully select neurons and trials from IBL dataset to analyze

--- Codes/Project/SX_analysis_pipeline.m
+++ Codes/Project/SX_analysis_pipeline.m
@@ -17,48 +17,66 @@
 %   - tuning curves (f(s))
 %   - and their corresponding uncertainties (sigmas)
 
-clear all; clc; close all
+clear all; clc; close all; format compact
 
-% 0. Load sorted spiking data
-load('spikeCount_LP_C00_R_N24_T281.mat')
+% 0. Set parameters
+str_region = 'CP';
+str_priorCond = 'bias0.5';
+nNeurons = 819; % Number of selected neurons
 
-% 0. Load behavioral data
-load('behavior_responses.mat')
-nTrials_withNOGO = length(behavior_responses);
+% str_contrast_all = {'C0', 'C0625', 'C125', 'C25', 'C50', 'C100'};
+contrast_all = [0, .0625, .125, .25, .5, 1];
+nContrasts = length(str_contrast_all);
 
-str_region = 'LP'; 
-str_contrast = 'C00'; 
-str_priorCond = 'R';
+contrasts_allC = [];
+spike_counts_allC = [];
+behavior_responses_allC = [];
 
-spike_counts = double(spike_counts');
-behavior_responses = double(behavior_responses);
+for iContrast = 1:nContrasts
+    contrast = contrast_all(iContrast); % Convert string to numerical contrast value
 
-% Filter out no-go trials
-spike_counts = spike_counts(:, behavior_responses ~= 0);
-behavior_responses = behavior_responses(behavior_responses ~= 0);
+    % 1a. Load sorted spiking data
+    load(sprintf('spikeCount_%s_%s_%s_N%d_T%d.mat', str_region, str_contrast, str_priorCond, nNeurons, nTrials))
+    load(sprintf('spikeCount_%s_%s_N%d_T%d.mat', str_region, str_priorCond, nNeurons, nTrials))
+    spike_counts = double(spike_counts');
 
-% 1. Extract parameters
-[nNeurons, nTrials] = size(spike_counts);  % Number of neurons
+    % 1b. Load behavioral data
+    load('behavior_responses.mat')
+    behavior_responses = double(behavior_responses);
 
-assert(nTrials == length(behavior_responses))
+    % Filter out no-go trials
+    nTrials_withNOGO = length(behavior_responses); assert(nTrials_withNOGO == nTrials);
+    spike_counts = spike_counts(:, behavior_responses ~= 0); assert(size(spike_counts, 2) == nTrials_withNOGO);
+    behavior_responses = behavior_responses(behavior_responses ~= 0);
+
+    % Concatenate
+    spike_counts_allC = [spike_counts_allC, spike_counts];
+    behavior_responses_allC = [behavior_responses_allC, behavior_responses];
+    contrasts_allC = [contrasts_allC, ones(1, nTrials) * contrast];
+end
 
 % Print data set info
 fprintf('Number of neurons: %d\n', nNeurons);
 fprintf('Number of trials: %d/%d (NO-GO included)\n', nTrials, nTrials_withNOGO);
 
+%% Derive tuning function
+
+
 %% Calculate CC and neural threshold
-CC_allN = nan(nNeurons, 1);
+CC_allN = nan(nNeurons, nContrastst);
 thresh_allN = nan(nNeurons, 1);
 for iN=1:nNeurons
-    % Calculate CC
-    CC_allN(iN) = corr(spike_counts(iN, :)', behavior_responses');
+    for iContrast = 1:nContrasts
+        % Calculate CC
+        CC_allN(iN, iContrast) = corr(spike_counts(iN, :)', behavior_responses');
 
-    % Calculate neural threshold
-    % should be divided by the mean firing rate; need the signal
-    thresh_allN(iN) = std(spike_counts(iN, :)) / mean(spike_counts(iN, :));
+        % Calculate neural threshold
+        % should be divided by the mean firing rate; need the signal
+        thresh_allN(iN) = std(spike_counts(iN, :)) / mean(spike_counts(iN, :));
+    end
 end
 
-% Visualize 
+% Visualize
 figure;
 plot(1./thresh_allN, CC_allN, 'o')
 xlabel('1 / Neural threshold');
